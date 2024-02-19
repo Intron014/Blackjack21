f585791bb4bc1aba506532b3eb937c23e5f22023
Jorge Benjumea <me@intron014.com>
2024-02-19 11:33:50 +0100

    Mucho texto

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-#include "cardIneitor.h"
+#include <stdlib.h>
+#include <time.h>
 #include "playerIneitor.h"
+#include "cardIneitor.h"
 
 int main() {
     // Init
+    srand(time(NULL));
     hand h[52];
     handGenerator(h);
 
     // Players
     int numPlay = 0;
     while(numPlay<2 || numPlay>4) {
-        printf("How many players are going to play? (2-4)\r");
+        printf("How many players are going to play? (2-4): ");
         scanf("%i", &numPlay);
     }
     player gaymers[numPlay];
@@ -26,18 +29,21 @@ int main() {
     int game = 1;
     while(game) {
         for (int i = 0; i < numPlay; i++) {
-            printf("Player %s, enter your bet: ", gaymers[i].name);
+            printf("Player %s's Turn\n> $%i\nEnter your bet: ", gaymers[i].name, gaymers[i].money);
             scanf("%i", &gaymers[i].bet);
         }
-        // for (int i = 0; i < numPlay; i++) {
-        //     playerHandReset(&gaymers[i]);
-        //     for (int j = 0; j < 2; j++) {
-        //         int card = rand() % 52
-        //         gaymers[i].hand[j].value = h[card].value;
-        //         strcpy(gaymers[i].hand[j].name, h[card].name);
-        //         h[card].howmany--;
-        //     }
-        // }
+        for (int i = 0; i < numPlay; i++) {
+            playerHandReset(&gaymers[i]);
+            int isAlive = 1;
+            autoFirstDeal(&gaymers[i], h);
+            for (int j = 2; isAlive ; j++) {
+                printf("Player %s's Turn\n", gaymers[i].name);
+                printf("Your hand (%i): \n", handValue(&gaymers[i]));
+                cardPrinter(gaymers[i].hand, j);
+                printf("Do you want another card? (1-Yes, 0-No): ");
+                scanf("%i", &isAlive);
+            }
+        }
     }
 
 }
